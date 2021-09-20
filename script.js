a87330ff26ae8ed62388dba13f23b24f3f5febd8
zatuazon <54414333+zatuazon@users.noreply.github.com>
2021-09-20 18:57:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ btnTimes.addEventListener('click', function(){
         navBar.classList.remove("show-nav");
     }
 })
+
+/* signin/register box pops into page*/
+var x=document.getElementById('login');
+var y=document.getElementById('register');
+var z=document.getElementById('btn');
+function register()
+{
+    x.style.left='-400px';
+    y.style.left='50px';
+    z.style.left='110px';
+}
+function login()
+{
+    x.style.left='50px';
+    y.style.left='450px';
+    z.style.left='0px';
+}
+
+/*makes signin/register window disappear if you click outside of box*/
+var modal = document.getElementById('login-form');
+window.onclick = function(event) 
+{
+    if (event.target == modal) 
+    {
+        modal.style.display = "none";
+    }
+}
